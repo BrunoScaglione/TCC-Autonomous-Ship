d2499ab049eddd8e8f3c3ee022fd3db5493c56a3
Bruno Scaglione <bruno.c.scaglione@gmail.com>
2021-08-29 04:46:19 -0300

    changed plots, added metrics; needto fix path error

--- surge_controller.py
+++ surge_controller.py
@@ -5,23 +5,57 @@ import numpy as np
 
 plt.rc('text', usetex=True)
 
-def getplots():
-    fig, axs = plt.subplots(3)
-    fig.suptitle(r"Surge velocity (1) and control input (2), through time")
+def getPlots():
+    fig, axs = plt.subplots(4)
+    fig.suptitle(r"Surge velocity, sliding variable, thrust, and rotation from top to bottom; through time")
     axs[0].plot(tspan, vetx)
     axs[0].ylabel(r"u [m/s]")
 
-    axs[1].plot(tspan, vetu)
-    axs[1].ylabel(r"\tau_1 [kN]")
+    axs[1].plot(tspan, vets)
+    axs[1].ylabel(r"s")
+
+    axs[2].plot(tspan, vettau)
+    axs[2].ylabel(r"\tau_1 [kN]")
 	
-    axs[2].plot(tspan, vetu)
-    axs[2].ylabel(r"n_p [Hz]")
-    axs[2].xlabel(r"t [s]")
+    axs[3].plot(tspan, vetnp)
+    axs[3].ylabel(r"n_p [Hz]")
+    axs[3].xlabel(r"t [s]")
 
     plt.savefig("results/pydyna_surge_control.png")
 
+def getMetrics(tspan, vetx, vets, vettau, vetnp):
+    idx_ts = numpy.where(vetx>0.02*(x0-(xd-phi))+(xd-phi))[0]
+    ts = tspan[idx_ts];
+
+    idx_ta = np.where(vets> -phi)[0];
+    ta = tspan[idx_ta];
+
+    overshoot = max(vetx) - xd
+
+    e_regime = vetx[-1]-xd;
+
+    x_slipstart = vetx[idx_ta];
+    x_tau = x_slipstart + 0.6321*(xd-x_slipstart);
+    idx_tau = np.where(vetx>x_tau)[0];
+    tau = tspan[idx_tau] - tspan[idx_ta];
+
+    tau_max = max(vettau)
+    np_max = max(vetnp)
+
+    metrics = {
+        "ts": ts,
+        "ta": ta,
+        'e': e,
+        "overshoot": overshoot,
+        "tau_max": tau_max,
+        "np_max": np_max
+    }
+
+    return metrics
+
+
 # converts trust force to rotation
-def getnp(tau: float, x:float) -> int:
+def getNp(tau: float, x:float) -> int:
     c1 = 0.0354
     c2 = 3.8107
     c3 = -0.0691 
@@ -31,7 +65,6 @@ def main():
     print('Starting...')
 
     try:
-        xd = 5 # desired surge velocity
         sim = pydyna.create_simulation('config/TankerL186B32_T085.p3d')
         ship = sim.vessels['0']
         propeller = ship.thrusters['0']
@@ -40,12 +73,14 @@ def main():
         t_end = 1000
         steps = t_end/dt + 1
 
-        # saturation parameter, 
-        # width of boundary layer or 
-        # steady-state error upper bound
-        phi = 0.01
+        phi = 0.01 # steady-state error upper bound
+        xd = 5 # desired surge velocity
+        m = 40415
+        X_added_mass = -3375
+        x0 = 0
 
         vetx = np.zeros((steps))
+        vets = np.zeros((steps))
         vetu = np.zeros((steps))
         vetnp = np.zeros((steps))
 
@@ -60,13 +95,16 @@ def main():
             sats = max(-1,min(s/phi,1))
             # input as function of x (control)
             u = x*abs(x)*(1.9091*(10**-4) + 5.2142*(10**-5)*sats)
+            # thrust
+            tau = u*(m - X_added_mass)
             # control allocation
-            np = getnp(u, x)
+            np = getNp(u, x)
             propeller.dem_rotation = np
 
             # save history
             vetx[i] = x
-            vetu[i] = u
+            vets[i] = s
+            vettau[i] = tau
             vetnp[i] = np
 
             # next step
@@ -75,7 +113,9 @@ def main():
         
         tspan = np.arange(0,t_end,dt)
         # plot u vs t (x vs t) and np vs t (u vs t)
-        getplots()
+        getPlots()
+        metrics = getMetrics()
+        print(metrics)
     except:
         print('Error loading simulation!')
         raise
