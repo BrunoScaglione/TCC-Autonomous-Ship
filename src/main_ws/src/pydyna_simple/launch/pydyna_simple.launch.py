b12a67f1a19704e0215f922c0ff673174bc0ae7f
Bruno Scaglione <bruno.c.scaglione@gmail.com>
2021-09-15 19:32:52 -0300

    added launch file for pydyna_simple

---
+++ src/main_ws/src/pydyna_simple/launch/pydyna_simple.launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='pydyna_simple',
+            executable='pydyna_simple',
+            name='pydyna_simple_node'
+        )
+    ])
