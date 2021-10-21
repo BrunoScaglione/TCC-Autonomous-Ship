20e0fd78d25a582713eb8cb8c87c29b27aa83769
Bruno Scaglione <bruno.c.scaglione@gmail.com>
2021-10-21 18:11:48 -0300

    gps_imu node working

--- notes/priorities.md
+++ notes/priorities.md
@@ -2,13 +2,21 @@
 # GUI will actually be venus
 # Need to make a route for setting different locations in the map
 # Make script to delete all logs and bags
+# change name gps_imu_simul to sensors_simul
 
-# Report: 
+# speculation:
+- zoh block after pydyna and before sensors_simul that publishes all the time (very high publishing rate, aproximating continuos), and put timer in the sensors, so that they read at their rate. Timer will be the period of 
+-the sensor rate, the callback you do processing on the data read by the subscriber callback (that is receiving at the publisher rate)
+- and the time published by pydyna should have t_step added where t_step is the time
+it takes for the computer to run one step of the simulation 
+
+# Report:      
 - remember to rewrite surge control part
 - change architectre figure: GUI and colors 
 - control allocation subscribes to state also
 - fix nomoto equation
 - chi_d - beta conflicting convention, since we are using chi_d + beta (sway  convention is different)
+- - not using gps_imu library anymore, using datasheets
 
 # TODOS
 - TODO: service for getting the updated waypoints from venus server
--- src/main_ws/src/path_following/path_following/gps_imu_simul.py
+++ src/main_ws/src/path_following/path_following/gps_imu_simul.py
@@ -1,4 +1,19 @@
+'''
+Datasheets used:
+    1. GPS 1: https://pdf.nauticexpo.com/pdf/san-jose-technology-inc/marine-gps-receiver/23414-76823.html
+    2. GPS 2 (just for velocity): {
+        https://www.u-blox.com/sites/default/files/products/documents/NEO-6_DataSheet_(GPS.G6-HW-09005).pdf or
+        https://docs.rs-online.com/c0e9/0900766b80df94d1.pdf or
+        https://www.generationrobots.com/media/GP-635T-121130-datasheet.pdf or
+        https://4.imimg.com/data4/SO/GH/MY-23669504/gps-shield-skg13c-module.pdf
+    }
+    3. IMU (indutrial): https://inertiallabs.com/wp-content/uploads/2020/09/IMU-P_Datasheet.rev3_.3_Sept_2020.pdf
+'''
+
 import sys
+import collections
+
+import numpy as np
 
 import rclpy
 from rclpy.node import Node
@@ -10,6 +25,29 @@ from path_following_interfaces.msg import State
 class GpsImuSimulator(Node):
     def __init__(self):
         super().__init__('gps_imu_simulator_node')
+
+        # params
+
+        # GPS_rate == 10 # [Hz] 
+        # IMU_rate == 2000 # [Hz]
+        # both rates are faster than the simulation 
+        # (10 - e Hz where e is due to the time do do 1 step of computations)
+        # therefore wont have sampling effect
+
+        # assuming sensors are already calibrated (no bias)
+        # assuming same variance for x and y, and 0 covariance
+        self.sigma_x = 5.46112744197 # GPS 1: from horizontal acc
+        self.sigma_y = 5.46112744197 # GPS 1: from horizontal acc
+        self.sigma_psi = 0.0523599 # GPS 1: from heading acc
+        self.sigma_u = None # is calculated dynamically 
+        self.sigma_v = None # is calculated dynamically
+        self.sigma_r = 0.0005 # IMU: from bias-in-run
+
+        self.sigma_xdot = 0.1 # GPS 2: from velocity acc
+        self.sigma_ydot = 0.1 # GPS 2: from velocity acc
+        
+        # State() has by default all zeros
+        self.last_two_states = collections.deque([State()], maxlen=2) 
 
         self.xs_msg = State()
 
@@ -35,12 +73,33 @@ class GpsImuSimulator(Node):
         
     def callback_state(self, msg):
         self.log_state(msg, 'subscriber')
+        self.last_two_states.appendleft(msg)
+        self.calculate_velocity_sigmas()
         simulated_state_msg = self.state_simul(msg)
         self.publisher_simulated_state.publish(simulated_state_msg)
         self.log_state(simulated_state_msg, 'publisher')
-    
+
+    def calculate_velocity_sigmas(self):
+        x_dot = ( list(self.last_two_states)[0].position.x - list(self.last_two_states)[1].position.x )/0.1
+        y_dot = ( list(self.last_two_states)[0].position.y - list(self.last_two_states)[1].position.y )/0.1
+        sigma_psi = self.sigma_psi
+        psi = list(self.last_two_states)[0].position.psi
+        # error propagation ([u, v].T = R@[xdot, ydot].T linear transformation or rotation matrix)
+        self.sigma_u = ( (np.cos(psi)*self.sigma_ydot)**2 + (np.sin(psi)*self.sigma_xdot)**2 \
+            + ((np.cos(psi)*x_dot - np.sin(psi)*y_dot)*sigma_psi)**2 )**0.5
+        self.sigma_v = ( (np.sin(psi)*self.sigma_ydot)**2 + (-np.cos(psi)*self.sigma_xdot)**2 \
+            + ((np.sin(psi)*x_dot + np.cos(psi)*y_dot)*sigma_psi)**2 )**0.5
+
     def state_simul(self, x):
-        return x # FILLER 
+        self.xs_msg.position.x = x.position.x + np.random.normal(0, self.sigma_x) # gps
+        self.xs_msg.position.y = x.position.y + np.random.normal(0, self.sigma_y) # gps
+        self.xs_msg.position.psi = x.position.psi + np.random.normal(0, self.sigma_psi) # gyrocompass
+
+        self.xs_msg.velocity.u = x.velocity.u + np.random.normal(0, self.sigma_u) # gps
+        self.xs_msg.velocity.v = x.velocity.v + np.random.normal(0, self.sigma_v) # gps
+        self.xs_msg.velocity.r = x.velocity.r + np.random.normal(0, self.sigma_r) # imu
+
+        return self.xs_msg
     
     def log_state(self, state, communicator):
         log_str = 'listened' if communicator == 'subscriber' else 'published simulated'
