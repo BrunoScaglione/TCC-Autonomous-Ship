805aafe7ed6de0a5db4fdd6c9b6e943d0b8eec42
PedroHMarzagao <phmarzagao@usp.br>
2021-11-16 19:33:50 -0300

    updates on wave filter

--- src/main_ws/src/build/path_following_interfaces/rosidl_generator_py/path_following_interfaces/msg/__init__.py
+++
@@ -1,6 +0,0 @@
-from path_following_interfaces.msg._control import Control  # noqa: F401
-from path_following_interfaces.msg._position import Position  # noqa: F401
-from path_following_interfaces.msg._positions_xy import PositionsXY  # noqa: F401
-from path_following_interfaces.msg._state import State  # noqa: F401
-from path_following_interfaces.msg._velocity import Velocity  # noqa: F401
-from path_following_interfaces.msg._waypoints import Waypoints  # noqa: F401
