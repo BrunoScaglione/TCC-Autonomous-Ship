56c4971a98fec0b1161e4d563fa3c8a030d52978
Bruno Scaglione <bruno.c.scaglione@gmail.com>
2021-12-27 02:23:23 -0300

    corrected configpublic.bat

--- helpers/client.bat
+++ helpers/client.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 call config.bat
-cd %scripts_path%
+cd %payloads_path%
 curl -H 'Content-Type: application/json' -X POST http://localhost:5000/waypoints -d @waypoints.json
 curl -H 'Content-Type: application/json' -X POST http://localhost:5000/initalCondition -d @start.json
 curl http://localhost:5000/start  
--- helpers/configpublic.bat
+++ helpers/configpublic.bat
@@ -1,9 +1,22 @@
-set setup_path="placeholder"
+@REM ROS2 installation directory, where setup.bat is located
+set underlay_path="placeholder"
+@REM ROS2 worskpace source directory
 set ws_src_path="placeholder"
-set local_setup_path="placeholder"
+@REM ROS2 workspace install directory, where local_setup.bat is located
+set overlay_path="placeholder"
+@REM ROS2 workspace directory
 set main_ws_path="placeholder"
+
+@REM pydyna_simple share directory: db folder
 set pydyna_pkg_db_path="placeholder"
-set path_pkg_logs_path="placeholder"
+@REM pydyna_simple share directory: db>rosbags folder
 set pydyna_pkg_rosbags_path="placeholder"
-set scripts_path="placeholder"
+
+@REM path_following share directory: logs folder
+set path_pkg_logs_path="placeholder"
+@REM path_following share directory: db folder
 set pydyna_pkg_db_path="placeholder"
+
+@REM directory where http json payloads are being kept (only when using curl)
+set payloads_path="placeholder" 
+
